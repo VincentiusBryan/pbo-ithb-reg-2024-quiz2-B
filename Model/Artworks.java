5c6bfd48105b417bd786d631e3407c88ea370917
VincentiusBryan <if-23018@students.ithb.ac.id>
2024-12-13 14:13:41 +0700

    first commit

---
+++ Model/Artworks.java
@@ -0,0 +1,8 @@
+package Model;
+
+public class Artworks {
+    int artworks_id;
+    String title;
+    String description;
+    String image_path;
+}
---
+++ Model/Users.java
@@ -0,0 +1,8 @@
+package Model;
+
+public class Users {
+    int user_id;
+    String name;
+    String email;
+    String password;
+}
